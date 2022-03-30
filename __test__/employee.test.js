0e39e0a1581385854619a36f27aaee0e38dc533e
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-03-29 22:55:34 -0500

    added employee test email and role

---
+++ __test__/employee.test.js
@@ -0,0 +1,25 @@
+const Employee = require("../lib/employee")
+
+describe("creates an employee object", () => {
+    const employee = new Employee("Willie Smitts", "15", "williesmittslaps");
+
+    expect(employee.name).toEqual(expect.any(String));
+
+    expect(employee.id).toEqual(expect.any(Number));
+
+    expect(employee.email).toEqual(expect.any(String));
+
+
+});
+
+describe('to get employee email', () => {
+    const employee = new Employee("Willie Smitts", "15", "williesmittslaps");
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+
+describe('to get employee role', () => {
+    const employee = new employee("Willie Smitts", "15", "williesmittslaps");
+
+    expect(employee.getRole()).toEqual("employee");
+})
